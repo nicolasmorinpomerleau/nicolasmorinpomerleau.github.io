11d5610f37a56ff840e707f3c365246123ca797d
nicolas <nicolasmorinpomerleau@gmail.com>
2019-08-03 17:32:23 -0400

    Many changes
    App.html
    search.js
    style.css

--- YTServer/javascript/Search.js
+++ YTServer/javascript/Search.js
@@ -67,8 +67,8 @@ function tplawesome(e,t){res=e;for(var n=0;n<t.length;n++){res=res.replace(/\{\{
 var scriptTime = new Array();
 //var scriptTimeHMS = new Array();
 var scriptText = new Array();
- scriptText = [" you test", " you test1"," you test2"," you test3"," you test4"," you test5","bou"];
-scriptTime = ["00:08:02", "00:02:13","00:03:01","00:14:01","00:15:01","00:17:01","00:16:01"];
+ scriptText = [" bla bla you bla bla test", " tou  bla you bla bla test1"," bla bla bla bla you"," hphv bla bla you bla test3"," bla bla bla bla test4"," ybla bla bla you test5","bou"," you   bla bla bla bla test13"," you test11"," you test10","bou"," ho oho you test", " you test1","  bla bla bla bla test2"," you test3"," you test4"," you test5","bou"," you test13"," you  bla bla bla bla  test11","h ehgl  you test10","bou"];
+scriptTime = ["00:08:02", "00:02:13","00:03:01","00:14:01","00:15:01","00:17:01","00:20:01","00:14:01","00:16:01","00:22:01","00:21:01","00:08:02", "00:02:13","00:33:01","00:34:01","00:35:01","00:37:01","00:30:01","00:34:01","00:36:01","00:32:01","00:31:01"];
 
 var parser, xmlDoc;
 var HTML_captions = "";
@@ -77,25 +77,111 @@ var ytApiKey = "AIzaSyCCf-S5kFXdDTWXc8cmCqBLXtjpt_5T18M";
 var YTTitle;
 
 var video_id = "";
+var VideoID;
+var player;
+var searchGlobal;
 
+//=========================  Angular =============================================
+//================================================================================
+var myApp = angular.module('YouTubeApp', []);
 
-var app = angular.module('YouTubeApp', []);
-app.controller('myCtrl', function($scope) {
-    $scope.$watch('VideoID', function(newValue, oldValue) {
-  if (newValue == oldValue) {
-    return;
-  }
-//  Call Change function
-    change();
-//  player.cueVideoById(scope.videoid);
+myApp.controller('YouTubeCtrl', function($scope) {
+  //initial settings
+  $scope.yt = {
+    width: 600, 
+    height: 480, 
+    videoid: ""
+  };
     
+  $scope.sendControlEvent = function (ctrlEvent) {
+    this.$broadcast(ctrlEvent);
+  }
 });
+
+myApp.directive('youtube', function($window) {
+  return {
+    restrict: "E",
+
+    scope: {
+      height: "@",
+      width: "@",
+      videoid: "@"
+    },
+
+    template: '<div></div>',
+
+    link: function(scope, element, attrs, $rootScope) {
+      var tag = document.createElement('script');
+      tag.src = "https://www.youtube.com/iframe_api";
+      var firstScriptTag = document.getElementsByTagName('script')[0];
+      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+      
+//      var player;
+
+      $window.onYouTubeIframeAPIReady = function() {
+
+        player = new YT.Player(element.children()[0], {
+          playerVars: {
+            autoplay: 0,
+            html5: 1,
+            theme: "light",
+            modesbranding: 0,
+            color: "white",
+            iv_load_policy: 3,
+            showinfo: 1,
+            controls: 1
+          },
+          
+          videoId: scope.videoid, 
+        });
+      };
+
+      scope.$watch('videoid', function(newValue, oldValue) {
+        if (newValue == oldValue) {
+          return;
+        }
+        change(scope.videoid);
+        player.cueVideoById(VideoID);
+      }); 
+    }  
+  };
 });
 
 
+//=========================  Angular =============================================
+//================================================================================
+//var app = angular.module('YouTubeApp', []);
+//app.controller('myCtrl', function($scope) {
+//    $scope.$watch('VideoID', function(newValue, oldValue) {
+//  if (newValue == oldValue) {
+//    return;
+//  }
+////  Call Change function
+//    change();
+////  player.cueVideoById(scope.videoid);
+//    
+//});
+//});
+//================================================================================
+//================================================================================
 
-function change (){
-    search = document.getElementById("search").value;
+
+function change (search){
+    
+//======================================
+//    Clean the seach result first
+//======================================
+    table = document.getElementById("tbl");
+    rows = table.getElementsByTagName("tr");
+    Nbrows = rows.length;
+
+    if(table && Nbrows != 0 ){
+                    $("#tbl").find("tr").remove();
+    }
+//======================================
+//======================================
+    
+//    search = document.getElementById("search").value;
     Length = search.length;
     video_id = search.indexOf("v=");
     ampersandPosition = search.indexOf("&");
@@ -103,12 +189,14 @@ function change (){
 
     if(ampersandPosition != -1) {
       video_id = search.substring(video_id+2, ampersandPosition);
-//            alert("changes !!  "+ video_id);
+      VideoID = video_id;
       run();
+
     }else{
       video_id = search.substring(video_id+2, Length);
-//                    alert("changes !!  "+ video_id);
+      VideoID = video_id;
       run();
+
     }
 };
                    
@@ -249,6 +337,7 @@ function fillData() {
 
 function runSearch () {
     search = document.getElementById("searchInYT").value;
+    searchGlobal = search;
     indexOF = new Array(); 
     
 //    Remove the old search result
@@ -295,7 +384,11 @@ function runSearch () {
       
     result = val.includes(":");
       if(result){
-          td.classList.add("cursor");
+          td.classList.add("ClasseTime");
+      }
+      else{
+          td.classList.add("ClasseText");
+          td.innerHTML = val.replace(searchGlobal,'<span style="font-weight: bold;">'+searchGlobal+'<\/span>');
       }
   }
 
@@ -347,17 +440,17 @@ $(document).click(function(event) {
 //===================================================================================
 
 //function onYouTubeIframeAPIReady() {
-function onYouTubeIframe (){
-    player = new YT.Player('video-placeholder', {
-        width: 600,
-        height: 400,
-        videoId: video_id,
-        playerVars: {
-            color: 'white',
-            playlist: 'taJ60kskkns,FG0fTKAqZ5g'
-        },
-//        events: {
-//            onReady: initialize
-//        }
-    });
-}
+//function onYouTubeIframe (){
+//    player = new YT.Player('video-placeholder', {
+//        width: 600,
+//        height: 400,
+//        videoId: video_id,
+//        playerVars: {
+//            color: 'white',
+//            playlist: 'taJ60kskkns,FG0fTKAqZ5g'
+//        },
+////        events: {
+////            onReady: initialize
+////        }
+//    });
+//}
